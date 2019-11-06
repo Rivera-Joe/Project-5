9c7f221e4fb048d6f63c4c012527400f7ac26560
GatheredFawn <57414099+GatheredFawn@users.noreply.github.com>
2019-11-05 20:59:37 -0600

    update
    js

--- Project 5/js/main.js
+++ Project 5/js/main.js
@@ -1,1 +1,23 @@
-//main js
+$(document).ready(function(){
+    $("nav").toggleClass("active")
+    $(".menu-toggle").click(function(){
+        $("nav").toggleClass("active")
+    })
+
+    var myIndex = 0;
+
+    function carousel() {
+      var i;
+      var x = document.getElementsByClassName("mySlides");
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+      }
+      myIndex++;
+      if (myIndex > x.length) {myIndex = 1}
+      x[myIndex-1].style.display = "block";
+      setTimeout(carousel, 3000); // Change image every 2 seconds
+    }
+    carousel();
+
+
+})

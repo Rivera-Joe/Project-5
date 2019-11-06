b3a921f28b94f8c4d6dfaf47c8d8907c9f6043e4
Rivera-Joe <36744600+Rivera-Joe@users.noreply.github.com>
2019-11-05 18:06:49 -0600

    Update table.php
    added function to add entries to table.
    table now can be added to database

--- Project 5/php/table.php
+++ Project 5/php/table.php
@@ -31,6 +31,8 @@ else{
 //create table
 $sql_createTable = "CREATE TABLE Animal_Table(ID INT AUTO_INCREMENT PRIMARY KEY, Name VARCHAR(20) NOT NULL, Species VARCHAR(10) NOT NULL, Breed VARCHAR(20) NOT NULL, Gender VARCHAR(1) NOT NULL, Color VARCHAR(15), Age VARCHAR(10), Size VARCHAR(1))";
 
+mysqli_select_db($connect, 'adoptionCenter') or die('Could not connect to database');
+
 //check for table creation
 //mysqli_query($connect, $sql_createTable) ? echo 'Table created successfully' : echo 'Error creating table: ' . mysqli_error($connect);
 if(mysqli_query($connect, $sql_createTable)){
@@ -38,6 +40,13 @@ if(mysqli_query($connect, $sql_createTable)){
 }
 else{
   echo 'Error creating table: ' . mysqli_error($connect);
+}
+
+
+function addEntry($name, $species, $breed, $gender, $color, $age, $size){
+
+  $sql_add = `INSERT INTO Animal_Table(Name, Species, Breed, Gender, Color, Age, Size) VALUES ({$name}, {$species}, {$breed}, {$gender}, {$color}, {$age}, {$size}`;
+  
 }
 
 

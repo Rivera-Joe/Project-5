a4f7b633b44c4d103b95986959981824c7192a02
Rivera-Joe <36744600+Rivera-Joe@users.noreply.github.com>
2019-11-12 13:48:14 -0600

    created add entry
    created add entry function that returns a value to a string and then the string is passed to the mysqli_query function to add the values to the table.

--- Project 5/php/table.php
+++ Project 5/php/table.php
@@ -10,11 +10,11 @@ $connect = mysqli_connect($server, $user);
 
 //check for connection
 if(!$connect){
-  die('Connection failed: ' . mysqli_connect_error());
+  die('Connection failed: ' . mysqli_connect_error($connect));
 }
 
 //if successful
-echo 'Connected Successfully';
+echo 'Connected Successfully<br />';
 
 //create database
 $sql_createDB = 'CREATE DATABASE adoptionCenter';
@@ -23,31 +23,47 @@ mysqli_query($connect, $sql_createDB);
 //check for database creation
 //mysqli_query($connect, $sql_createDB) ? echo 'Database created successfully' : echo 'Error creating database: ' . mysqli_error($connect);
 if(mysqli_query($connect, $sql_createDB)){
-  echo 'Created database successfully';
+  echo 'Created database successfully<br />';
 }
 else{
   echo 'Error creating database: ' . mysqli_error($connect);
 }
 //create table
-$sql_createTable = "CREATE TABLE Animal_Table(ID INT AUTO_INCREMENT PRIMARY KEY, Name VARCHAR(20) NOT NULL, Species VARCHAR(10) NOT NULL, Breed VARCHAR(20) NOT NULL, Gender VARCHAR(1) NOT NULL, Color VARCHAR(15), Age VARCHAR(10), Size VARCHAR(1))";
+$sql_createTable = "CREATE TABLE Animal_Table(ID INT AUTO_INCREMENT PRIMARY KEY, Name VARCHAR(20) NOT NULL, Species VARCHAR(10) NOT NULL, Breed VARCHAR(30) NOT NULL, Gender VARCHAR(1) NOT NULL, Color VARCHAR(15), Age VARCHAR(10), Size VARCHAR(1))";
+$sql_dbName = 'adoptioncenter';
 
-mysqli_select_db($connect, 'adoptionCenter') or die('Could not connect to database');
+mysqli_select_db($connect, $sql_dbName) or die('Could not connect to database<br />');
 
-//check for table creation
-//mysqli_query($connect, $sql_createTable) ? echo 'Table created successfully' : echo 'Error creating table: ' . mysqli_error($connect);
 if(mysqli_query($connect, $sql_createTable)){
-  echo 'Created table successfully';
+  echo 'Table created successfully<br />';
 }
 else{
-  echo 'Error creating table: ' . mysqli_error($connect);
+  echo('Could not create table: ' . mysqli_error($connect));
 }
 
+function addEntry($name, $spec, $breed, $gender, $color, $age, $size){
 
-function addEntry($name, $species, $breed, $gender, $color, $age, $size){
 
-  $sql_add = `INSERT INTO Animal_Table(Name, Species, Breed, Gender, Color, Age, Size) VALUES ({$name}, {$species}, {$breed}, {$gender}, {$color}, {$age}, {$size}`;
-  
+
+
+
+      $sql_add = "INSERT INTO Animal_Table(Name, Species, Breed, Gender, Color, Age, Size) VALUES('$name', '$spec', '$breed', '$gender', '$color', '$age', '$size')";
+
+      return $sql_add;
+
+
+
+};
+
+$entryString = addEntry('Bullet', 'Dog', 'Grey_Hound','M', 'White', '1_Year', 'M');
+
+
+if(mysqli_query($connect, $entryString)){
+  echo "<br />Record added successfully";
+}else{
+  echo "<br />Error: " . mysqli_error($connect);
 }
+mysqli_close($connect);
 
 
 
@@ -61,8 +77,4 @@ function addEntry($name, $species, $breed, $gender, $color, $age, $size){
 
 
 
-
-
-
-
-mysqli_close($connect); ?>
+ ?>
